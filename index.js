b8cbfaf25f6971a5071c95552a7861889dd56f9e
Hector Cald <hi.hector20@gmail.com>
2024-09-08 18:54:32 -0400

    cambios de navegacion y seccion Inicio terminada

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function secciones(){
         seccionNosotros.style.display = 'none';
         seccionProductos.style.display = 'none';
         seccionRecetas.style.display = 'none';
-        
+        marcas();
     }
 }
 function desabilitaEnlaces(){
@@ -48,129 +48,45 @@ function navegacion(){
     const seccionProductos = document.querySelector('.seccionProductos');
     const seccionRecetas = document.querySelector('.seccionRecetas');
     const seccionNosotros = document.querySelector('.seccionNosotros');
-    const pestañaInicio = document.querySelector('.pestañaInicio');
+
     let links = document.querySelectorAll('.link');
     let seccion;
-
-    if(window.innerWidth > 800){
         links.forEach(element => {
             element.addEventListener('click', function(){
-                links.forEach(btn => {
-                    btn.style.color = 'black';
-                    btn.style.textDecoration = 'none';
-                });
-                this.style.color = 'red'
-                this.style.textDecoration = 'underline';
                 if(element.textContent==='Productos'){
                     seccionProductos.style.display = 'flex'
                     seccionNosotros.style.display = 'none';
                     seccionInicio.style.display = 'none';
                     seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
                 }
                 else if(element.textContent==='Recetas'){
                     seccionRecetas.style.display = 'flex'
                     seccionNosotros.style.display = 'none';
                     seccionInicio.style.display = 'none';
                     seccionProductos.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
                 }
                 else if(element.textContent==='Inicio'){
                     seccionInicio.style.display = 'flex'
                     seccionNosotros.style.display = 'none';
                     seccionProductos.style.display = 'none';
                     seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
                 }
                 else if(element.textContent==='Nosotros'){
                     seccionNosotros.style.display = 'flex'
                     seccionProductos.style.display = 'none';
                     seccionInicio.style.display = 'none';
                     seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
                 }
+                menu();
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                })
+                marcas();
             })
+            
         });
     }
-    else{
-        links.forEach(element => {
-            element.addEventListener('click', function(){
-                links.forEach(btn => {
-                    btn.style.backgroundColor = 'white';
-                    btn.style.color = 'black';
-                });
-                this.style.backgroundColor = 'gray'
-                this.style.color = 'white';
-                if(element.textContent==='Productos'){
-                    seccionProductos.style.display = 'flex'
-                    seccionNosotros.style.display = 'none';
-                    seccionInicio.style.display = 'none';
-                    seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                }
-                else if(element.textContent==='Recetas'){
-                    seccionRecetas.style.display = 'flex'
-                    seccionNosotros.style.display = 'none';
-                    seccionInicio.style.display = 'none';
-                    seccionProductos.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
-                }
-                else if(element.textContent==='Inicio'){
-                    seccionInicio.style.display = 'flex'
-                    seccionNosotros.style.display = 'none';
-                    seccionProductos.style.display = 'none';
-                    seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
-                }
-                else if(element.textContent==='Nosotros'){
-                    seccionNosotros.style.display = 'flex'
-                    seccionProductos.style.display = 'none';
-                    seccionInicio.style.display = 'none';
-                    seccionRecetas.style.display = 'none';
-                    menu();
-                    window.scrollTo({
-                        top: 0,
-                        behavior: 'smooth'
-                    })
-                    
-                }
-            })
-        });
-    }
-}
 function contacto(){
     let links = document.querySelectorAll('.link');
     window.scrollTo({
@@ -191,4 +107,132 @@ function menu(){
 }
 function catalogo(){
     window.location.href = './public/catalogo.pdf'
+}
+function botonProductos(){
+    const seccionInicio = document.querySelector('.seccionInicio');
+    const seccionProductos = document.querySelector('.seccionProductos');
+    seccionProductos.style.display = 'flex'
+    seccionInicio.style.display = 'none';
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+    marcas();
+}
+function botonNosotros(){
+    const seccionInicio = document.querySelector('.seccionInicio');
+    const seccionNosotros = document.querySelector('.seccionNosotros');
+    seccionNosotros.style.display = 'flex'
+    seccionInicio.style.display = 'none';
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+    marcas();
+}
+function marcas(){
+    var seccionInicio = document.querySelector('.seccionInicio');
+    var seccionProductos = document.querySelector('.seccionProductos');
+    var seccionRecetas = document.querySelector('.seccionRecetas');
+    var seccionNosotros = document.querySelector('.seccionNosotros');
+
+    const pestañaInicio = document.querySelector('.pestañaInicio');
+    const pestañaProductos = document.querySelector('.pestañaProductos');
+    const pestañaRecetas = document.querySelector('.pestañaRecetas');
+    const pestañaNosotros = document.querySelector('.pestañaNosotros');
+    
+
+    if(window.innerWidth>800){
+        if(seccionInicio.style.display==='flex'){
+            pestañaInicio.style.color = 'red'
+            pestañaNosotros.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaInicio.style.textDecoration = 'underline'
+            pestañaNosotros.style.textDecoration = 'none'
+            pestañaProductos.style.textDecoration= 'none'
+            pestañaRecetas.style.textDecoration = 'none'
+        }
+        else if(seccionNosotros.style.display==='flex'){
+            pestañaNosotros.style.color = 'red'
+            pestañaInicio.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaNosotros.style.textDecoration = 'underline'
+            pestañaInicio.style.textDecoration = 'none'
+            pestañaProductos.style.textDecoration= 'none'
+            pestañaRecetas.style.textDecoration = 'none'
+        }
+        else if(seccionRecetas.style.display==='flex'){
+            pestañaRecetas.style.color = 'red'
+            pestañaNosotros.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaInicio.style.color = 'black'
+    
+            pestañaRecetas.style.textDecoration = 'underline'
+            pestañaNosotros.style.textDecoration = 'none'
+            pestañaProductos.style.textDecoration= 'none'
+            pestañaInicio.style.textDecoration = 'none'
+        }
+        else if(seccionProductos.style.display==='flex'){
+            pestañaProductos.style.color = 'red'
+            pestañaNosotros.style.color = 'black'
+            pestañaInicio.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaProductos.style.textDecoration = 'underline'
+            pestañaNosotros.style.textDecoration = 'none'
+            pestañaInicio.style.textDecoration= 'none'
+            pestañaRecetas.style.textDecoration = 'none'
+        }
+    }
+    else{
+        if(seccionInicio.style.display==='flex'){
+            pestañaInicio.style.color = 'white'
+            pestañaNosotros.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaInicio.style.backgroundColor = 'gray'
+            pestañaNosotros.style.backgroundColor = 'white'
+            pestañaProductos.style.backgroundColor= 'white'
+            pestañaRecetas.style.backgroundColor = 'white'
+        }
+        else if(seccionNosotros.style.display==='flex'){
+            pestañaNosotros.style.color = 'white'
+            pestañaInicio.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaNosotros.style.backgroundColor = 'gray'
+            pestañaRecetas.style.backgroundColor = 'white'
+            pestañaProductos.style.backgroundColor= 'white'
+            pestañaInicio.style.backgroundColor = 'white'
+        }
+        else if(seccionRecetas.style.display==='flex'){
+            pestañaRecetas.style.color = 'white'
+            pestañaNosotros.style.color = 'black'
+            pestañaProductos.style.color = 'black'
+            pestañaInicio.style.color = 'black'
+    
+            pestañaRecetas.style.backgroundColor = 'gray'
+            pestañaNosotros.style.backgroundColor = 'white'
+            pestañaProductos.style.backgroundColor= 'white'
+            pestañaInicio.style.backgroundColor = 'white'
+        }
+        else if(seccionProductos.style.display==='flex'){
+            pestañaProductos.style.color = 'white'
+            pestañaNosotros.style.color = 'black'
+            pestañaInicio.style.color = 'black'
+            pestañaRecetas.style.color = 'black'
+    
+            pestañaProductos.style.backgroundColor = 'gray'
+            pestañaNosotros.style.backgroundColor = 'white'
+            pestañaRecetas.style.backgroundColor= 'white'
+            pestañaInicio.style.backgroundColor = 'white'
+        }
+    }
+   
 }
