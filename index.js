7ef0a8b53eef7dfea0e75c74e7d8035cfded7ccb
Hector Cald <hi.hector20@gmail.com>
2024-09-08 18:09:56 -0400

    cambios en el catalogo

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', function(){
     desabilitaEnlaces();
     navegacion();
     secciones();
-    
 })
 function secciones(){
     const seccionInicio = document.querySelector('.seccionInicio');
     const seccionProductos = document.querySelector('.seccionProductos');
     const seccionRecetas = document.querySelector('.seccionRecetas');
     const seccionNosotros = document.querySelector('.seccionNosotros');
+    const pestañaInicio = document.querySelector('.pestañaInicio');
+
 
     const cuadroMenu = document.querySelector('.menu');
     let secciones = document.querySelectorAll('.seccion');
@@ -26,9 +27,11 @@ function secciones(){
         })
     });
     window.onload = function(){
+        seccionInicio.style.display ='flex'
         seccionNosotros.style.display = 'none';
         seccionProductos.style.display = 'none';
         seccionRecetas.style.display = 'none';
+        
     }
 }
 function desabilitaEnlaces(){
@@ -45,58 +48,131 @@ function navegacion(){
     const seccionProductos = document.querySelector('.seccionProductos');
     const seccionRecetas = document.querySelector('.seccionRecetas');
     const seccionNosotros = document.querySelector('.seccionNosotros');
+    const pestañaInicio = document.querySelector('.pestañaInicio');
     let links = document.querySelectorAll('.link');
     let seccion;
-    links.forEach(element => {
-        element.addEventListener('click', function(){
-            if(element.textContent==='Productos'){
-                seccionProductos.style.display = 'flex'
-                seccionNosotros.style.display = 'none';
-                seccionInicio.style.display = 'none';
-                seccionRecetas.style.display = 'none';
-                menu();
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-            else if(element.textContent==='Recetas'){
-                seccionRecetas.style.display = 'flex'
-                seccionNosotros.style.display = 'none';
-                seccionInicio.style.display = 'none';
-                seccionProductos.style.display = 'none';
-                menu();
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-            else if(element.textContent==='Inicio'){
-                seccionInicio.style.display = 'flex'
-                seccionNosotros.style.display = 'none';
-                seccionProductos.style.display = 'none';
-                seccionRecetas.style.display = 'none';
-                menu();
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-            else if(element.textContent==='Nosotros'){
-                seccionNosotros.style.display = 'flex'
-                seccionProductos.style.display = 'none';
-                seccionInicio.style.display = 'none';
-                seccionRecetas.style.display = 'none';
-                menu();
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-        })
-    });
+
+    if(window.innerWidth > 800){
+        links.forEach(element => {
+            element.addEventListener('click', function(){
+                links.forEach(btn => {
+                    btn.style.color = 'black';
+                    btn.style.textDecoration = 'none';
+                });
+                this.style.color = 'red'
+                this.style.textDecoration = 'underline';
+                if(element.textContent==='Productos'){
+                    seccionProductos.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                }
+                else if(element.textContent==='Recetas'){
+                    seccionRecetas.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionProductos.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+                else if(element.textContent==='Inicio'){
+                    seccionInicio.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionProductos.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+                else if(element.textContent==='Nosotros'){
+                    seccionNosotros.style.display = 'flex'
+                    seccionProductos.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+            })
+        });
+    }
+    else{
+        links.forEach(element => {
+            element.addEventListener('click', function(){
+                links.forEach(btn => {
+                    btn.style.backgroundColor = 'white';
+                    btn.style.color = 'black';
+                });
+                this.style.backgroundColor = 'gray'
+                this.style.color = 'white';
+                if(element.textContent==='Productos'){
+                    seccionProductos.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                }
+                else if(element.textContent==='Recetas'){
+                    seccionRecetas.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionProductos.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+                else if(element.textContent==='Inicio'){
+                    seccionInicio.style.display = 'flex'
+                    seccionNosotros.style.display = 'none';
+                    seccionProductos.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+                else if(element.textContent==='Nosotros'){
+                    seccionNosotros.style.display = 'flex'
+                    seccionProductos.style.display = 'none';
+                    seccionInicio.style.display = 'none';
+                    seccionRecetas.style.display = 'none';
+                    menu();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                    
+                }
+            })
+        });
+    }
 }
 function contacto(){
+    let links = document.querySelectorAll('.link');
     window.scrollTo({
         top: document.body.scrollHeight, behavior: 'smooth'
     });
@@ -112,4 +188,7 @@ function menu(){
     else{
         imagen.src = './public/Imagenes/menu_open.png';
     }
+}
+function catalogo(){
+    window.location.href = './public/catalogo.pdf'
 }
